bc81c290fd9d8ab830bab4778e288439ff77d5dd
ma-sch <developer@online-schneider.net>
2024-05-08 11:33:53 +0200

    Add Sonatype maven repo

--- src/main/java/org/eclipse/slm/monitoring/prometheus/aas/submodel/PlatformResourcesSubmodelService.java
+++ src/main/java/org/eclipse/slm/monitoring/prometheus/aas/submodel/PlatformResourcesSubmodelService.java
@@ -5,6 +5,8 @@ import org.eclipse.digitaltwin.aas4j.v3.model.Submodel;
 import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelElement;
 import org.eclipse.digitaltwin.basyx.core.exceptions.CollidingIdentifierException;
 import org.eclipse.digitaltwin.basyx.core.exceptions.ElementDoesNotExistException;
+import org.eclipse.digitaltwin.basyx.core.exceptions.ElementNotAFileException;
+import org.eclipse.digitaltwin.basyx.core.exceptions.FileDoesNotExistException;
 import org.eclipse.digitaltwin.basyx.core.pagination.CursorResult;
 import org.eclipse.digitaltwin.basyx.core.pagination.PaginationInfo;
 import org.eclipse.digitaltwin.basyx.core.pagination.PaginationSupport;
@@ -15,6 +17,8 @@ import org.eclipse.digitaltwin.basyx.submodelservice.value.SubmodelElementValue;
 import org.eclipse.digitaltwin.basyx.submodelservice.value.factory.SubmodelElementValueMapperFactory;
 import org.eclipse.slm.monitoring.prometheus.aas.exceptions.MethodNotImplementedException;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.List;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
@@ -86,6 +90,21 @@ public class PlatformResourcesSubmodelService implements SubmodelService {
 
 	@Override
 	public OperationVariable[] invokeOperation(String idShortPath, OperationVariable[] input) {
+		throw new MethodNotImplementedException();
+	}
+
+	@Override
+	public File getFileByPath(String idShortPath) throws ElementDoesNotExistException, ElementNotAFileException, FileDoesNotExistException {
+		throw new MethodNotImplementedException();
+	}
+
+	@Override
+	public void setFileValue(String idShortPath, String fileName, InputStream inputStream) throws ElementDoesNotExistException, ElementNotAFileException {
+		throw new MethodNotImplementedException();
+	}
+
+	@Override
+	public void deleteFileValue(String idShortPath) throws ElementDoesNotExistException, ElementNotAFileException, FileDoesNotExistException {
 		throw new MethodNotImplementedException();
 	}
 
